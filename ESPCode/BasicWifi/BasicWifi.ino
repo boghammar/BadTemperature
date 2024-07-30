811b0b3c023a19db11e9224e7a26d5ad67c9a2f0
Anders Boghammar <boghammar@users.noreply.github.com>
2024-07-30 21:56:11 +0200

    Added MQTT that works with open non TLS brokers

--- ESPCode/BasicWifi/BasicWifi.ino
+++ ESPCode/BasicWifi/BasicWifi.ino
@@ -6,7 +6,7 @@
 
 const char* ssid = "fullrulle";
 const char* password = "0123456789";
-const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* mqtt_server = "test.mosquitto.org"; //"broker.mqtt-dashboard.com";
 
 String serverURL = "http://192.168.2.90:3000";
 
@@ -17,45 +17,51 @@ ESP8266WiFiMulti WiFiMulti;
 // WiFi connect timeout per AP. Increase when connecting takes longer.
 const uint32_t connectTimeoutMs = 5000;
 
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+// ---------------------------------------------------------------------------------
+// Utility functions declarations
+// ---------------------------------------------------------------------------------
+void StartWifi();
 void PostSignal();
 void GetStatus();
+void MQTTMessageReceived(char* topic, byte* payload, unsigned int length);
+void MQTTReconnect();
 
-
+// ---------------------------------------------------------------------------------
+// My Setup
+// ---------------------------------------------------------------------------------
 void setup() {
-  // Don't save WiFi configuration in flash - optional
-  WiFi.persistent(false);
-
   Serial.begin(115200);
   Serial.println("");
   
   pinMode(LED_BUILTIN, OUTPUT);
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP(ssid, password);
-  //WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while (WiFiMulti.run(connectTimeoutMs) != WL_CONNECTED) {
-  //while(WiFi.status() != WL_CONNECTED) { 
-    Serial.printf(".%d",WiFi.status());
-    delay(500);
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network as ");
-  Serial.print(WiFi.hostname());
-  Serial.print(" with IP Address: ");
-  Serial.print(WiFi.localIP());
-  Serial.print(" RSSI: ");
-  Serial.print(WiFi.RSSI());
-  Serial.println();
+  mqttClient.setServer(mqtt_server, 1883);
+  mqttClient.setCallback(MQTTMessageReceived);
+  mqttClient.setKeepAlive(20); // Default 15
+
+  StartWifi();
 }
 
+// ---------------------------------------------------------------------------------
+// My Loop
+// ---------------------------------------------------------------------------------
 void loop() {
+
+  if (!mqttClient.connected()) {
+    MQTTReconnect();
+  }
+  mqttClient.loop();
+
   // Do some LED blinking to see that it has been downloaded and works
   //
   unsigned long currentMillis = millis();
 
   // ----------------------------------- Do some blinking to see that we are alive
   if (currentMillis - previousBlink >= blinkIntervall) {
+    mqttClient.publish("fullTopic/MyWorld","hello world");
     previousBlink = currentMillis;
     //delay(5000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED on by making the voltage LOW
@@ -80,13 +86,37 @@ void loop() {
 
 
 }
+// ---------------------------------------------------------------------------------
+// Utility functions
+// ---------------------------------------------------------------------------------
+  void StartWifi() {
+    // Don't save WiFi configuration in flash - optional
+    WiFi.persistent(false);
 
+    WiFi.mode(WIFI_STA);
+    WiFiMulti.addAP(ssid, password);
+    //WiFi.begin(ssid, password);
+    Serial.println("Connecting");
+    while (WiFiMulti.run(connectTimeoutMs) != WL_CONNECTED) {
+    //while(WiFi.status() != WL_CONNECTED) { 
+      Serial.printf(".%d",WiFi.status());
+      delay(500);
+    }
+    Serial.println("");
+    Serial.print("Connected to WiFi network as ");
+    Serial.print(WiFi.hostname());
+    Serial.print(" with IP Address: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(" RSSI: ");
+    Serial.print(WiFi.RSSI());
+    Serial.println();
+  }
+// ---------------------------------------------------------------------------------
 void PostSignal() {
-    WiFiClient client;
     HTTPClient http;
     String serverpath = serverURL + "/signal";
     Serial.printf("Doing a post for %s\r\n", serverpath.c_str());
-    http.begin(client, serverpath.c_str());
+    http.begin(wifiClient, serverpath.c_str());
     http.addHeader("Content-Type", "application/json");
 
     JSONVar jObj;
@@ -115,12 +145,12 @@ void PostSignal() {
     http.end();
 }
 
+// ---------------------------------------------------------------------------------
 void GetStatus() {
-      WiFiClient client;
       HTTPClient http;
       String serverpath = serverURL + "/status";
       Serial.printf("Doing a get for %s\r\n", serverpath.c_str());
-      http.begin(client, serverpath.c_str());
+      http.begin(wifiClient, serverpath.c_str());
 
       // Send HTTP GET request
       int httpResponseCode = http.GET();
@@ -137,4 +167,36 @@ void GetStatus() {
       }
       
       http.end();
+}
+// ---------------------------------------------------------------------------------
+void MQTTMessageReceived(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+// ---------------------------------------------------------------------------------
+void MQTTReconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect("arduinoClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttClient.publish("fullTopic","hello world");
+      // ... and resubscribe
+      mqttClient.subscribe("fullTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
 }
