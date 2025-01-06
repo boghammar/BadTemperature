fb4579d65ecbd3c7bfed23c0144877ebf015f3b5
Anders Boghammar <boghammar@users.noreply.github.com>
2025-01-06 19:38:55 +0100

    Added LORA code for the ESP32 LORA boards

---
+++ ESPCode/HeltecWirelessStick/HeltecWirelessStick/HeltecWirelessStick.ino
@@ -0,0 +1,322 @@
+/*
+ * BadTemperature for the Heltec Wireless Stick V3
+ *    based on the Heltec FactoryTest for this device
+ *
+ * boghammar.com 2025
+ */
+#include "Arduino.h"
+#include "WiFi.h"
+#include "LoRaWan_APP.h"
+#include <Wire.h>  
+#include "HT_SSD1306Wire.h"
+
+/********************************* lora  *********************************************/
+#define RF_FREQUENCY                                868000000 // Hz
+ 
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 30 // Define the payload size here
+
+/********************************* lora declarations *********************************************/
+static RadioEvents_t RadioEvents;
+
+typedef enum {
+    LOWPOWER,
+    STATE_RX,
+    STATE_TX
+} States_t;
+
+char txpacket[BUFFER_SIZE];
+char rxpacket[BUFFER_SIZE];
+
+int16_t txNumber;
+int16_t rxNumber;
+States_t state;
+bool sleepMode = false;
+int16_t Rssi,rxSize;
+
+String rssi = "RSSI --";
+String packSize = "--";
+String packet;
+String send_num;
+String show_lora = "lora data show";
+
+unsigned int counter = 0;
+
+bool receiveflag = false; // software flag for LoRa receiver, received data makes it true.
+
+long lastSendTime = 0;        // last send time
+int interval = 1000;          // interval between sends
+int16_t RssiDetection = 0;
+
+// -------------------------------------------------------------------- 
+// LORA handling
+// -------------------------------------------------------------------- 
+void OnTxDone( void ) {
+  	Serial.print("TX done......");
+	  state=STATE_RX;
+
+}
+// -------------------------------------------------------------------- 
+void OnTxTimeout( void ) {
+    Radio.Sleep( );
+    Serial.print("TX Timeout......");
+	  state=STATE_TX;
+}
+// -------------------------------------------------------------------- 
+void OnRxDone( uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr ) {
+	  rxNumber++;
+    Rssi=rssi;
+    rxSize=size;
+    memcpy(rxpacket, payload, size );
+    rxpacket[size]='\0';
+    Radio.Sleep( );
+
+    Serial.printf("\r\nreceived packet \"%s\" with Rssi %d , length %d\r\n",rxpacket,Rssi,rxSize);
+    Serial.println("wait to send next packet");
+	  receiveflag = true;
+    state=STATE_TX;
+}
+// -------------------------------------------------------------------- 
+void lora_init(void) {
+    Mcu.begin(HELTEC_BOARD,SLOW_CLK_TPYE);
+    txNumber=0;
+    Rssi=0;
+	  rxNumber = 0;
+    RadioEvents.TxDone = OnTxDone;
+    RadioEvents.TxTimeout = OnTxTimeout;
+    RadioEvents.RxDone = OnRxDone;
+
+    Radio.Init( &RadioEvents );
+    Radio.SetChannel( RF_FREQUENCY );
+    Radio.SetTxConfig( MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+                                   LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+                                   LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   true, 0, 0, LORA_IQ_INVERSION_ON, 3000 );
+
+    Radio.SetRxConfig( MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+                                   LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+                                   LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+                                   0, true, 0, 0, LORA_IQ_INVERSION_ON, true );
+	  
+    // Starting state is transmitt
+    state=STATE_TX;
+}
+// -------------------------------------------------------------------- 
+void lora_send(void) {
+	txNumber++;
+	sprintf(txpacket,"hello %d, Rssi : %d",txNumber,Rssi);
+	Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
+	Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+}
+// -------------------------------------------------------------------- 
+// Display utils
+// -------------------------------------------------------------------- 
+
+SSD1306Wire  factory_display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_64_32, RST_OLED); // addr , freq , i2c group , resolution , rst
+
+// -------------------------------------------------------------------- 
+// WiFi utils
+// -------------------------------------------------------------------- 
+void WIFISetUp(void) {
+	// Set WiFi to station mode and disconnect from an AP if it was previously connected
+	WiFi.disconnect(true);
+	delay(100);
+	WiFi.mode(WIFI_STA);
+	WiFi.setAutoReconnect(true);
+	WiFi.begin("Ompabompa","Bonnie23");   //fill in "Your WiFi SSID","Your Password"
+	delay(100);
+
+	byte count = 0;
+	while(WiFi.status() != WL_CONNECTED && count < 10) {
+		count ++;
+		delay(500);
+		factory_display.drawString(0, 0, "Connecting...");
+		factory_display.display();
+	}
+	factory_display.clear();
+
+	if(WiFi.status() == WL_CONNECTED) {
+		factory_display.drawString(0, 8, "OK.");
+		factory_display.display();
+	} else {
+		factory_display.clear();
+		factory_display.drawString(0, 8, "Failed");
+		factory_display.display();
+	}
+
+	factory_display.drawString(0, 16, "WIFI Setup done");
+	factory_display.display();
+	delay(500);
+}
+
+// -------------------------------------------------------------------- 
+// GPIO0 (User switch) handling
+// -------------------------------------------------------------------- 
+bool resendflag=false;
+bool deepsleepflag=false;
+bool interrupt_flag = false;
+// -------------------------------------------------------------------- 
+void interrupt_GPIO0() {
+	interrupt_flag = true;
+}
+// -------------------------------------------------------------------- 
+void interrupt_handle(void) {
+	if(interrupt_flag) {
+		interrupt_flag = false;
+		if(digitalRead(0)==0) {
+			if(rxNumber <=2) { // If we have not receivde more then 2 messages => resend
+				resendflag=true;
+			} else {           // else we go to deepsleep
+				deepsleepflag=true;
+			}
+		}
+	}
+}
+// -------------------------------------------------------------------- 
+// VEXT utils
+// -------------------------------------------------------------------- 
+void VextON(void)
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, LOW);
+  
+}
+
+void VextOFF(void) //Vext default OFF
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
+
+// -------------------------------------------------------------------------------------- 
+// -------------------------------------------------------------------- Standard setup
+// -------------------------------------------------------------------------------------- 
+void setup() {
+  Serial.begin(115200);
+  uint64_t chipid=ESP.getEfuseMac();    //The chip ID is essentially its MAC address(length: 6 bytes).
+	Serial.printf("ESP32ChipID=%04X",(uint16_t)(chipid>>32));   //print High 2 bytes
+	Serial.printf("%08X\n",(uint32_t)chipid);                   //print Low 4bytes.
+
+	VextON();
+	delay(100);
+  lora_init();
+
+  // ------------------------ Init display
+	factory_display.init();
+  factory_display.clear();
+  factory_display.display();
+
+  factory_display.drawString(0, 0, "Test Start");
+	factory_display.display();
+  factory_display.clear();
+
+  Serial.println("Display initialized. Setting up WiFi");
+
+  // ------------------------ Init WiFi
+	WIFISetUp();
+  factory_display.clear();
+
+  // Attach interupthandler to user switch (GPIO0)
+  attachInterrupt(0,interrupt_GPIO0,FALLING);
+
+  // ------------------------ Ready to go
+	packet ="Setup done";
+  factory_display.drawString(0, 0, packet);
+  factory_display.display();
+  delay(100);
+  factory_display.clear();
+	pinMode(LED ,OUTPUT);
+	digitalWrite(LED, LOW);  
+
+}
+
+// -------------------------------------------------------------------------------------- 
+// -------------------------------------------------------------------- Standard loop
+// -------------------------------------------------------------------------------------- 
+void loop() {
+  // Check user switch has been pushed first
+  interrupt_handle();
+
+  if (deepsleepflag) { // --- Going to deepsleep
+    Serial.println("Going to deepsleep");
+    VextOFF();
+    Radio.Sleep();
+    pinMode(RADIO_DIO_1,ANALOG);
+    pinMode(RADIO_NSS,ANALOG);
+    pinMode(RADIO_RESET,ANALOG);
+    pinMode(RADIO_BUSY,ANALOG);
+    pinMode(LORA_CLK,ANALOG);
+    pinMode(LORA_MISO,ANALOG);
+    pinMode(LORA_MOSI,ANALOG);
+    pinMode(LED,ANALOG);
+    pinMode(Vext,ANALOG);
+    detachInterrupt(0);
+    Wire.end();
+    Serial.end();//true);
+    SPI.end();
+    WiFi.mode(WIFI_OFF);
+    esp_sleep_enable_timer_wakeup(600*1000*(uint64_t)1000); //Sleep for 600 seconds (10 min)
+    esp_deep_sleep_start();
+  }
+
+  // ----------------------------- Display send/recv status
+  if (receiveflag) {
+    receiveflag = false; // signal that we have read incomming 
+    packet = "R:";
+    int i = 0;
+    while(i < rxSize) {
+      packet += rxpacket[i];
+      i++;
+    }
+    packSize = "Rssi: ";
+    packSize += String(Rssi,DEC);
+    send_num = "S: ";
+    send_num += String(txNumber,DEC);
+	  factory_display.drawString(0, 0, "LoRa");
+    factory_display.drawString(0, 10, packet);
+    factory_display.drawString(0, 20, packSize);
+    factory_display.drawString(32, 0, send_num);
+    factory_display.display();
+    delay(10);
+    factory_display.clear();
+
+  }
+  // ----------------------------- Handle current state
+  switch(state) {
+      case STATE_TX:
+        delay(1000);
+        txNumber++;
+        sprintf(txpacket,"hello %d,Rssi:%d",txNumber,Rssi);
+        Serial.printf("\r\nsending packet \"%s\" , length %d\r\n",txpacket, strlen(txpacket));
+        Radio.Send( (uint8_t *)txpacket, strlen(txpacket) );
+        state=LOWPOWER;
+        break;
+      case STATE_RX:
+        Serial.println("into RX mode");
+        Radio.Rx( 0 );
+        state=LOWPOWER;
+        break;
+      case LOWPOWER:
+        Radio.IrqProcess( );
+        break;
+      default:
+        break;
+    }
+}
---
+++ ESPCode/TTGOSender/src/main.cpp
@@ -0,0 +1,197 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/ttgo-lora32-sx1276-arduino-ide/
+*********/
+
+//Libraries for LoRa
+#include <SPI.h>
+#include <LoRa.h>
+
+//Libraries for OLED Display
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "Arduino.h"
+
+// -------------------- Define the pins used by the LoRa transceiver module
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 23 //14
+#define DIO0 26
+
+// -------------------- Define the LoRa settings
+#define BAND 868000000 //866E6
+#define TX_OUTPUT_POWER                             14        // dBm
+
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       7         // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+// -------------------- Define the pins used by the OLED pins
+#define OLED_SDA 21 //4
+#define OLED_SCL 22 //15 
+#define OLED_RST -1 //16
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+void onReceive(int packetSize);
+void sendMessage(String message);
+
+int rssi = 0;
+//packet counter
+int counter = 0;
+String msg  = "";
+String lastInMsg = "";
+long lastSendTime = 0;        // last send time
+int interval = 2000;          // interval between sends
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
+
+// ---------------------------------------------------------- Standard setup
+void setup() {
+  //initialize Serial Monitor
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("LoRa with TTGO LoRa32 V2.1");
+
+  // ------------------ reset OLED display via software
+  pinMode(OLED_RST, OUTPUT);
+  digitalWrite(OLED_RST, LOW);
+  delay(20);
+  digitalWrite(OLED_RST, HIGH);
+
+  // ------------------ initialize OLED
+  Wire.begin(OLED_SDA, OLED_SCL);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  Serial.println("OLED init done");
+
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("LORA SENDER ");
+  display.display();
+  
+  // ------------------ SPI LoRa pins
+  SPI.begin(SCK, MISO, MOSI, SS);
+  // ------------------ setup LoRa transceiver module
+  LoRa.setPins(SS, RST, DIO0);
+  
+  if (!LoRa.begin(BAND)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+
+  // ------------------ configure LoRa settings
+  // LoRa.setTxPower(TX_OUTPUT_POWER);
+  // LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
+  // LoRa.setSignalBandwidth(LORA_BANDWIDTH);
+  // LoRa.setCodingRate4(LORA_CODINGRATE);
+  // LoRa.setPreambleLength(LORA_PREAMBLE_LENGTH);
+  // LoRa.setSymbolTimeout(LORA_SYMBOL_TIMEOUT);
+  // LoRa.setFixLengthPayloadOn(LORA_FIX_LENGTH_PAYLOAD_ON);
+  // LoRa.setIqInversion(LORA_IQ_INVERSION_ON);
+  // ------------------ start listening for incomming packets
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+
+  Serial.println("LoRa Initializing OK!");
+  display.setCursor(0,10);
+  display.print("LoRa Initializing OK!");
+  display.display();
+  delay(2000);
+}
+
+// ---------------------------------------------------------- Standard loop
+void loop() {
+ 
+  
+  if (millis() - lastSendTime > interval) {
+    Serial.print("Sending packet: ");
+
+    msg = "anders " + String(counter);
+    Serial.println(msg);
+    sendMessage(msg);
+    lastSendTime = millis();            // timestamp the message
+    interval = random(2000) + 1000;     // 2-3 seconds
+    LoRa.receive();                     // go back into receive mode
+    counter++;
+  }
+
+  // ------------------ display send/recv status  
+  String tmp = "";
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.println("LORA TRANSCEIVER");
+  
+  display.setCursor(0,20);
+  display.setTextSize(1);
+  tmp = "S: " + msg;
+  display.print(tmp);
+  
+  display.setCursor(0,30);
+  tmp = "R: " + lastInMsg;
+  display.print(tmp);
+  
+  display.setCursor(0,40);
+  display.print("Rssi:");
+  display.print(rssi);
+  
+  display.display();
+
+  
+  //delay(500);
+}
+
+// ----------------------------------------------------------------------------
+// LORA FUNCTIONS
+// ----------------------------------------------------------------------------
+// ---------------------------------------------------------- Send a message
+void sendMessage(String message) {
+  // Send LoRa packet to receiver
+  LoRa.beginPacket();
+  LoRa.print(message);
+  LoRa.endPacket();
+}
+// ---------------------------------------------------------- Recieve callback
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  // int recipient = LoRa.read();          // recipient address
+  // byte sender = LoRa.read();            // sender address
+  // byte incomingMsgId = LoRa.read();     // incoming msg ID
+  // byte incomingLength = LoRa.read();    // incoming msg length
+
+  lastInMsg = "";                 // payload of packet
+
+  while (LoRa.available()) {            // can't use readString() in callback, so
+    lastInMsg += (char)LoRa.read();      // add bytes one by one
+  }
+
+  rssi = LoRa.packetRssi();
+  Serial.print("Received packet '");
+  Serial.print(lastInMsg);
+  Serial.print("' with RSSI ");
+  Serial.println(rssi);
+  // if (incomingLength != incoming.length()) {   // check length for error
+  //   Serial.println("error: message length does not match length");
+  //   return;                             // skip rest of function
+  // }
+
+}
